5b1baf17b13ef1def8d96d3dadb96455051244f2
Roman Tarassenko <tarassenko@me.com>
2022-11-20 16:27:33 +0300

    - Убран лишний код и импорты

--- numbers_api/urls.py
+++ numbers_api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from numbers_api.views import echo, get_token_view, CalculationsView, add_view, subtract_view, multiply_view, \
+from numbers_api.views import CalculationsView, add_view, subtract_view, multiply_view, \
     divide_view
 
 urlpatterns = [
-    path('echo/', echo, name="echo"),
-    path('get-token/', get_token_view, name="get_token"),
     path('calc/', CalculationsView.as_view(), name="calculations"),
     path('add/', add_view, name="add"),
     path('subtract/', subtract_view, name="subtract"),
--- numbers_api/views.py
+++ numbers_api/views.py
@@ -1,32 +1,7 @@
 import json
 
-from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
-from django.shortcuts import render
-from django.utils.datetime_safe import datetime
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http import JsonResponse
 from django.views.generic import TemplateView
-
-
-# from django.utils.datetime_safe import datetime
-
-
-# Create your views here.
-def echo(request, *args, **kwargs):
-    answer = {
-        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        'method': request.method,
-    }
-    answer_as_json = json.dumps(answer)
-    response = HttpResponse(answer_as_json)
-    response['Content-Type'] = 'application/json'
-    return response
-
-
-@ensure_csrf_cookie
-def get_token_view(request, *args, **kwargs):
-    if request.method == 'GET':
-        return HttpResponse()
-    return HttpResponseNotAllowed(f'Not allowed Method {request.method}')
 
 
 def add_view(request, *args, **kwargs):

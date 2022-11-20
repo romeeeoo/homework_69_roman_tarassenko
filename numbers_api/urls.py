85989383740bdb5f3ef5973f28d1d47aa0f5d098
Roman Tarassenko <tarassenko@me.com>
2022-11-20 16:23:36 +0300

    -Добавлены представления для остальных вариантов вычисления.
    - Добавлены пути
    
    - ajax запрос через jquery функцию принимает адрес кнопки через this

--- numbers_api/urls.py
+++ numbers_api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from numbers_api.views import echo, get_token_view, CalculationsView, add_view
+from numbers_api.views import echo, get_token_view, CalculationsView, add_view, subtract_view, multiply_view, \
+    divide_view
 
 urlpatterns = [
     path('echo/', echo, name="echo"),
     path('get-token/', get_token_view, name="get_token"),
     path('calc/', CalculationsView.as_view(), name="calculations"),
     path('add/', add_view, name="add"),
-    # path('calc/', CalculationsView.as_view(), name="calculations"),
-    # path('calc/', CalculationsView.as_view(), name="calculations"),
-    # path('calc/', CalculationsView.as_view(), name="calculations"),
+    path('subtract/', subtract_view, name="subtract"),
+    path('multiply/', multiply_view, name="multiply"),
+    path('divide/', divide_view, name="divide")
 ]
--- numbers_api/views.py
+++ numbers_api/views.py
@@ -35,13 +35,63 @@ def add_view(request, *args, **kwargs):
         try:
             a = float(data.get("A"))
             b = float(data.get("B"))
-            my_sum = a + b
-            return JsonResponse({"result": my_sum})
-        except TypeError as e:
-            response = JsonResponse({'error': f'{e}'})
+            result = a + b
+            return JsonResponse({"result": result})
+        except ValueError:
+            response = JsonResponse({"error": "wrong type on input"})
             response.status_code = 400
+            print(response.content)
             return response
 
+
+def subtract_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            a = float(data.get("A"))
+            b = float(data.get("B"))
+            result = a - b
+            return JsonResponse({"result": result})
+        except ValueError:
+            response = JsonResponse({"error": "wrong type on input"})
+            response.status_code = 400
+            print(response.content)
+            return response
+
+
+def multiply_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            a = float(data.get("A"))
+            b = float(data.get("B"))
+            result = a * b
+            return JsonResponse({"result": result})
+        except ValueError:
+            response = JsonResponse({"error": "wrong type on input"})
+            response.status_code = 400
+            print(response.content)
+            return response
+
+
+def divide_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            a = float(data.get("A"))
+            b = float(data.get("B"))
+            result = a / b
+            return JsonResponse({"result": result})
+        except ValueError:
+            response = JsonResponse({"error": "wrong type on input"})
+            response.status_code = 400
+            print(response.content)
+            return response
+        except ZeroDivisionError:
+            response = JsonResponse({"error": "zero division is not allowed"})
+            response.status_code = 400
+            print(response.content)
+            return response
 
 
 class CalculationsView(TemplateView):
